f19d723b28709e620785bc6e1c2a6eba4fb2f440
Alexander Orlov <79202875363@ya.ru>
2017-04-07 14:04:35 +0300

    Написал, постейший интефейс приложения.

--- app/src/main/java/com/example/alex/yandextranslator/MainActivity.java
+++ app/src/main/java/com/example/alex/yandextranslator/MainActivity.java
@@ -2,12 +2,51 @@ package com.example.alex.yandextranslator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView textView;
+    private EditText editText;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        buttonBehavior();
     }
+
+    private void buttonBehavior() {
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                String text_to_yandex = getEditText(editText);
+                break;
+        }
+    }
+
+    private void initView() {
+        editText = (EditText)findViewById(R.id.edit_text);
+        button = (Button)findViewById(R.id.button);
+        textView = (TextView)findViewById(R.id.text_view);
+    }
+
+    private String getEditText(EditText editText){
+        String text = editText.getText().toString();
+        if (text.length() == 0) text = "";
+        return text;
+    }
+
+
 }
