d322ebdaa598e715f943058e9695087e58c057bb
Alexander Orlov <79202875363@ya.ru>
2017-04-23 17:52:57 +0300

    Сделал вывод избранного во фрагмент.

---
+++ app/src/main/java/com/example/alex/yandextranslator/adapter/RecyclerViewFavoriteAdapter.java
@@ -0,0 +1,155 @@
+package com.example.alex.yandextranslator.adapter;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.alex.yandextranslator.R;
+import com.example.alex.yandextranslator.data.Contract;
+
+/**
+ * Created by alex on 22.04.17.
+ */
+
+public class RecyclerViewFavoriteAdapter extends
+        RecyclerView.Adapter<RecyclerViewFavoriteAdapter.ViewHolder>{
+    private final String LOG_TAG = this.getClass().getSimpleName();
+
+    private Context context;
+
+    private Cursor dataCursor;
+
+    public RecyclerViewFavoriteAdapter(Context context,
+                                       Cursor cursor) {
+        this.context = context;
+        this.dataCursor = cursor;
+    }
+
+    public Cursor swapCursor (Cursor cursor){
+        if (dataCursor == cursor){
+            return null;
+        }
+
+        Cursor oldCursor = dataCursor;
+        this.dataCursor = cursor;
+        if (cursor != null){
+            this.notifyDataSetChanged();
+        }
+        return oldCursor;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.d(LOG_TAG, "Start onCreateViewHolder");
+        View view = LayoutInflater.from(context).inflate(R.layout.list_view_item_history_layout,
+                parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Log.d(LOG_TAG, "Start onBindViewHolder");
+
+        dataCursor.moveToPosition(position);
+
+        int idColIndex = dataCursor.getColumnIndex(Contract.HistoryFavorites.COLUMN_ID);
+        int translatableTextColIndex = dataCursor.getColumnIndex(Contract.HistoryFavorites.
+                COLUMN_TRANSLATABLE_TEXT);
+        int translatedTextColIndex = dataCursor.getColumnIndex(Contract.HistoryFavorites.
+                COLUMN_TRANSLATED_TEXT);
+        int translationDirectionColIndex = dataCursor.getColumnIndex(Contract.HistoryFavorites.
+                COLUMN_TRANSLATION_DIRECTION);
+        int favoriteColIndex = dataCursor.getColumnIndex(Contract.HistoryFavorites.
+                COLUMN_FAVORITE);
+
+        int id = dataCursor.getInt(idColIndex);
+        String translatableFromCursor = dataCursor.getString(translatableTextColIndex);
+        String translatedFromCursor = dataCursor.getString(translatedTextColIndex);
+        String translationDirectionFromCursor = dataCursor.
+                getString(translationDirectionColIndex);
+        int favorite = dataCursor.getInt(favoriteColIndex);
+        boolean favoriteFromCursor = castIntToBoolean(favorite);
+
+        holder.textViewTranslatableText.setText(translatableFromCursor);
+        holder.textViewTranslatedText.setText(translatedFromCursor);
+        holder.textViewTranslationDirection.setText(translationDirectionFromCursor);
+        holder.textViewFavorite.setText(context.getString(R.string.select_favorites));
+
+        holder.textViewFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (holder.textViewFavorite.getText().toString().equals
+                        (context.getString(R.string.un_select_favorites))){
+                    holder.textViewFavorite.setText(context.getString(R.string.select_favorites));
+                } else {
+                    holder.textViewFavorite.setText(context.getString(R.string.un_select_favorites));
+                }
+                selectFavoriteToHistoryFavoritesTable(id, favoriteFromCursor);
+            }
+        });
+    }
+
+    private void selectFavoriteToHistoryFavoritesTable(int id, boolean favorite){
+        int intFavorite = castBooleanToInt(favorite);
+        ContentValues cv = new ContentValues();
+        cv.put(Contract.HistoryFavorites.COLUMN_FAVORITE, intFavorite);
+
+        String selection = Contract.HistoryFavorites.COLUMN_ID + " = ?";
+        String[] selectionArgs = {Integer.toString(id)};
+
+        context.getContentResolver().update(Contract.HistoryFavorites.CONTENT_URI, cv, selection,
+                selectionArgs);
+    }
+
+    private int castBooleanToInt(boolean favorite) {
+        if (favorite) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    private boolean castIntToBoolean(int favoriteFromCursor){
+        if (favoriteFromCursor == 0){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d(LOG_TAG, "Start getItemCount");
+        return (dataCursor == null) ? 0 : dataCursor.getCount();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewFavorite;
+        TextView textViewTranslatableText;
+        TextView textViewTranslatedText;
+        TextView textViewTranslationDirection;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            initView(itemView);
+        }
+
+        private void initView(View itemView){
+            Log.d(LOG_TAG, "Start initView");
+            textViewFavorite = (TextView)itemView.findViewById(R.id.text_view_favorite);
+            textViewTranslatableText = (TextView)itemView.findViewById(R.id.
+                    text_view_translatable_text);
+            textViewTranslatedText = (TextView)itemView.findViewById(R.id.text_view_translated_text);
+            textViewTranslationDirection = (TextView)itemView.findViewById(R.id.
+                    text_view_translation_direction);
+        }
+    }
+}
--- app/src/main/java/com/example/alex/yandextranslator/fragment/TabFragmentFavorites.java
+++ app/src/main/java/com/example/alex/yandextranslator/fragment/TabFragmentFavorites.java
@@ -1,10 +1,107 @@
 package com.example.alex.yandextranslator.fragment;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.example.alex.yandextranslator.App;
+import com.example.alex.yandextranslator.R;
+import com.example.alex.yandextranslator.adapter.RecyclerViewFavoriteAdapter;
+import com.example.alex.yandextranslator.adapter.RecyclerViewHistoryAdapter;
+import com.example.alex.yandextranslator.data.Contract;
+import com.example.alex.yandextranslator.model.HistoryFavorites;
+
+import java.util.ArrayList;
 
 /**
  * Created by alex on 20.04.17.
  */
 
-public class TabFragmentFavorites extends Fragment {
+public class TabFragmentFavorites extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private final String LOG_TAG = this.getClass().getSimpleName();
+
+    private EditText editTextSearch;
+    private RecyclerView recyclerViewFavorite;
+    private RecyclerViewFavoriteAdapter adapter;
+
+    private App app;
+
+    private int LOADER_ID = 2;
+
+    @Override
+    public void onAttach(Context context) {
+        Log.d(LOG_TAG, "Start onAttach");
+        super.onAttach(context);
+        app = ((App)getActivity().getApplicationContext());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "Start onCreateView");
+
+        View view = inflater.inflate(R.layout.history_tab_fragment_layout, container, false);
+
+        initView(view);
+
+        initRecyclerView();
+
+        return view;
+    }
+
+    private void initView(View view){
+        editTextSearch = (EditText)view.findViewById(R.id.edit_text_search);
+        recyclerViewFavorite = (RecyclerView)view.findViewById(R.id.recycler_view_history);
+    }
+
+    private void initRecyclerView(){
+        Log.d(LOG_TAG, "Start initRecyclerView");
+        ArrayList<HistoryFavorites> arrayList = app.getHistoryFavoritesArrayList();
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        adapter = new RecyclerViewFavoriteAdapter(getActivity(), null);
+        recyclerViewFavorite.addItemDecoration(new DividerItemDecoration(getActivity(),
+                DividerItemDecoration.VERTICAL));
+        recyclerViewFavorite.setAdapter(adapter);
+        recyclerViewFavorite.setLayoutManager(layoutManager);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(LOADER_ID, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String selection = Contract.HistoryFavorites.COLUMN_FAVORITE + " = ?";
+        String[] selectionArgs = {Integer.toString(1)};
+
+        return new CursorLoader(getActivity(),
+                Contract.HistoryFavorites.CONTENT_URI, null, selection, selectionArgs, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        adapter.swapCursor(null);
+    }
 }
